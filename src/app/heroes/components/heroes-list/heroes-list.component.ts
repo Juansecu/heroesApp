f0939410f9af223cfb6f4f995fbcaa307cc4cffc
Juansecu <juansecoder@hotmail.com>
2021-07-21 16:15:20 -0500

    Created layout to display heroes information through a list

--- src/app/heroes/components/heroes-list/heroes-list.component.ts
+++ src/app/heroes/components/heroes-list/heroes-list.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../../typings/Hero';
+
+import { HeroesService } from '../../services/heroes.service';
+
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
   styles: [
-  ]
+    `
+      mat-card {
+        margin-top: 2rem;
+      }
+    `,
+  ],
 })
 export class HeroesListComponent implements OnInit {
+  heroes!: Hero[];
 
-  constructor() { }
+  constructor(private readonly heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesService
+      .getHeroes()
+      .subscribe((response) => (this.heroes = response));
   }
-
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Hero } from '../typings/Hero';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
+  private readonly url = 'http://localhost:3000/heroes';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.url);
+  }
 }
