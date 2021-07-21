a95d9f409d272e0ef0280f41a011eac6ad72f873
Juansecu <juansecoder@hotmail.com>
2021-07-21 15:06:34 -0500

    Marked some properties as optional

--- src/app/heroes/typings/Hero.d.ts
+++ src/app/heroes/typings/Hero.d.ts
@@ -1,10 +1,11 @@
 export interface Hero {
-  id: string;
+  id?: string;
   superhero: string;
   publisher: Publisher;
   alter_ego: string;
   first_appearance: string;
   characters: string;
+  image?: string;
 }
 
 export enum Publisher {
