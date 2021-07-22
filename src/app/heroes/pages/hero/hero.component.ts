7d165bd6aa50b4a005e851998466ec6a5b4d8fa0
Juansecu <juansecoder@hotmail.com>
2021-07-22 11:14:41 -0500

    Displaying some information about hero in HeroComponent

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Hero } from '../../typings/Hero';
+
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styles: [
-  ]
+    `
+      img {
+        width: 100%;
+        border-radius: 10px;
+      }
+    `,
+  ],
 })
 export class HeroComponent implements OnInit {
+  hero!: Hero;
 
-  constructor() { }
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router,
+    private readonly heroesService: HeroesService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ heroId }) => this.heroesService.getHero(heroId)))
+      .subscribe((hero) => (this.hero = hero));
   }
 
+  goBack(): void {
+    this.router.navigate(['heroes', 'heroes']);
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
+
 import { Hero } from '../typings/Hero';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
-  private readonly url = 'http://localhost:3000/heroes';
+  private readonly baseUrl = `${environment.serverUrl}/heroes`;
 
   constructor(private readonly http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.url);
+    return this.http.get<Hero[]>(this.baseUrl);
+  }
+
+  getHero(heroId: string): Observable<Hero> {
+    return this.http.get<Hero>(`${this.baseUrl}/${heroId}`);
   }
 }
